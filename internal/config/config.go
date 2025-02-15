67da1353936c86e6b29532c5519bae41134c507c
Roman Savchyshyn <roma.fln137@gmail.com>
2025-02-15 12:22:57 +0200

    make hours configurable (#17)

--- internal/config/config.go
+++ internal/config/config.go
@@ -19,16 +19,23 @@ const (
 	EnvProd = "prod"
 
 	defaultPublishInterval = 10 * time.Minute
+	defaultHourFrom        = 9
+	defaultHourTo          = 21
 )
 
 type (
-	Config struct {
-		Env             string
-		TelegramToken   string
-		AllowedChatIDs  []int64
-		DBURL           string
+	WordCheckSchedule struct {
 		PublishInterval time.Duration
+		HourFrom        int
+		HourTo          int
 		Location        *time.Location
+	}
+	Config struct {
+		Env               string
+		TelegramToken     string
+		AllowedChatIDs    []int64
+		DBURL             string
+		WordCheckSchedule WordCheckSchedule
 	}
 
 	configBuilderFn func(string) (*Config, error)
@@ -65,16 +72,12 @@ func GetConfig() (*Config, error) {
 		return nil, err
 	}
 	res.Env = env
-	res.Location = loc
-	return validate(res, nil)
+	res.WordCheckSchedule.Location = loc
+	return validate(res)
 }
 
-func validate(conf *Config, err error) (*Config, error) {
-	if err != nil {
-		return nil, err
-	}
-
-	errs := make([]string, 0, 6) //nolint:mnd // 6 is a reasonable default value
+func validate(conf *Config) (*Config, error) {
+	errs := make([]string, 0, 10) //nolint:mnd // 10 is a reasonable default value
 	if conf.Env != EnvDev && conf.Env != EnvProd {
 		errs = append(errs, fmt.Sprintf("unknown environment: %s", conf.Env))
 	}
@@ -87,10 +90,19 @@ func validate(conf *Config, err error) (*Config, error) {
 	if conf.DBURL == "" {
 		errs = append(errs, "db url is required")
 	}
-	if conf.PublishInterval == 0 {
+	if conf.WordCheckSchedule.PublishInterval == 0 {
 		errs = append(errs, "publish interval is required")
 	}
-	if conf.Location == nil {
+	if conf.WordCheckSchedule.HourFrom < 0 || conf.WordCheckSchedule.HourFrom > 23 {
+		errs = append(errs, fmt.Sprintf("hour from %d must be in range 0-23", conf.WordCheckSchedule.HourFrom))
+	}
+	if conf.WordCheckSchedule.HourTo < 0 || conf.WordCheckSchedule.HourTo > 23 {
+		errs = append(errs, fmt.Sprintf("hour to %d must be in range 0-23", conf.WordCheckSchedule.HourTo))
+	}
+	if conf.WordCheckSchedule.HourFrom >= conf.WordCheckSchedule.HourTo {
+		errs = append(errs, fmt.Sprintf("hour from %d must be less than hour to %d", conf.WordCheckSchedule.HourFrom, conf.WordCheckSchedule.HourTo))
+	}
+	if conf.WordCheckSchedule.Location == nil {
 		errs = append(errs, "location is required")
 	}
 
@@ -112,12 +124,24 @@ func getDevConfig(string) (*Config, error) {
 	if err != nil {
 		return nil, fmt.Errorf("parse publish interval: %w", err)
 	}
+	wordCheckScheduleFrom, err := parseInt(os.Getenv("WORD_CHECK_SCHEDULE_FROM"), defaultHourFrom)
+	if err != nil {
+		return nil, fmt.Errorf("parse word check schedule from: %w", err)
+	}
+	wordCheckScheduleTo, err := parseInt(os.Getenv("WORD_CHECK_SCHEDULE_TO"), defaultHourTo)
+	if err != nil {
+		return nil, fmt.Errorf("parse word check schedule to: %w", err)
+	}
 
 	return &Config{
-		TelegramToken:   telegramTokenEnvVar,
-		AllowedChatIDs:  allowedChatIDs,
-		DBURL:           dbURLEnvVar,
-		PublishInterval: publishInterval,
+		TelegramToken:  telegramTokenEnvVar,
+		AllowedChatIDs: allowedChatIDs,
+		DBURL:          dbURLEnvVar,
+		WordCheckSchedule: WordCheckSchedule{
+			PublishInterval: publishInterval,
+			HourFrom:        wordCheckScheduleFrom,
+			HourTo:          wordCheckScheduleTo,
+		},
 	}, nil
 }
 
@@ -135,7 +159,9 @@ func getProdConfig(region string) (*Config, error) {
 			aws.String("/english-learning-bot/prod/telegram-token"),
 			aws.String("/english-learning-bot/prod/allowed-chat-ids"),
 			aws.String("/english-learning-bot/prod/db-url"),
-			aws.String("/english-learning-bot/prod/publish-interval"),
+			aws.String("/english-learning-bot/prod/word-check-schedule/publish-interval"),
+			aws.String("/english-learning-bot/prod/word-check-schedule/hour-from"),
+			aws.String("/english-learning-bot/prod/word-check-schedule/hour-to"),
 		},
 		WithDecryption: aws.Bool(true),
 	})
@@ -147,6 +173,8 @@ func getProdConfig(region string) (*Config, error) {
 	var allowedChatIDs []int64
 	dbURL := ""
 	publishIntervalStr := ""
+	scheduleHourFrom := defaultHourFrom
+	scheduleHourTo := defaultHourTo
 	for _, param := range parameters.Parameters {
 		switch *param.Name {
 		case "/english-learning-bot/prod/telegram-token":
@@ -160,6 +188,16 @@ func getProdConfig(region string) (*Config, error) {
 			dbURL = *param.Value
 		case "/english-learning-bot/prod/publish-interval":
 			publishIntervalStr = *param.Value
+		case "/english-learning-bot/prod/word-check-schedule/hour-from":
+			scheduleHourFrom, err = strconv.Atoi(*param.Value)
+			if err != nil {
+				return nil, fmt.Errorf("parse word check schedule from: %w", err)
+			}
+		case "/english-learning-bot/prod/word-check-schedule/hour-to":
+			scheduleHourTo, err = strconv.Atoi(*param.Value)
+			if err != nil {
+				return nil, fmt.Errorf("parse word check schedule to: %w", err)
+			}
 		}
 	}
 
@@ -169,10 +207,14 @@ func getProdConfig(region string) (*Config, error) {
 	}
 
 	return &Config{
-		TelegramToken:   telegramToken,
-		AllowedChatIDs:  allowedChatIDs,
-		DBURL:           dbURL,
-		PublishInterval: publishInterval,
+		TelegramToken:  telegramToken,
+		AllowedChatIDs: allowedChatIDs,
+		DBURL:          dbURL,
+		WordCheckSchedule: WordCheckSchedule{
+			PublishInterval: publishInterval,
+			HourFrom:        scheduleHourFrom,
+			HourTo:          scheduleHourTo,
+		},
 	}, nil
 }
 
@@ -199,4 +241,11 @@ func parsePublishInterval(publishIntervalStr string, def time.Duration) (time.Du
 		return def, nil
 	}
 	return time.ParseDuration(publishIntervalStr)
+}
+
+func parseInt(val string, def int) (int, error) {
+	if val == "" {
+		return def, nil
+	}
+	return strconv.Atoi(val)
 }
